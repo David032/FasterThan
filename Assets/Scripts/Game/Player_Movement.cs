91cf25581bac82867ffd38b274bc1b05101af1d1
Kai Tooley <kai.p.o.tooley@gmail.com>
2019-11-22 12:16:10 +0000

    start seting up player
    player movement and update player prefab

--- Assets/Scripts/Game/Player_Movement.cs
+++ Assets/Scripts/Game/Player_Movement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    public float playerSpeed = 3;
+
+    Rigidbody playerBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") < 0.0f)
+        {
+            playerBody.AddRelativeForce(Vector3.left * playerSpeed);
+        }
+        else if (Input.GetAxis("Horizontal") > 0.0f)
+        {
+            playerBody.AddRelativeForce(Vector3.right * playerSpeed);
+        }
+
+        if (Input.GetAxis("Vertical") < 0.0f)
+        {
+            playerBody.AddRelativeForce(Vector3.back * playerSpeed);
+        }
+        else if (Input.GetAxis("Vertical") > 0.0f)
+        {
+            playerBody.AddRelativeForce(Vector3.forward * playerSpeed);
+        }
+
+        if (Input.GetAxis("Mouse X") != 0)
+        {
+            playerBody.AddTorque(Vector3.up * Input.GetAxis("Mouse X"));
+        }
     }
 }
