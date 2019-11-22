b187a8422b8dadecc4ecaf1e4cf6c3c89af290fc
Jake <Pokepeel15@yahoo.co.uk>
2019-11-22 14:31:02 +0000

    Fixed floor generator
    Added features to generate the floor in editor. need to add materials.

---
+++ Assets/Scripts/Game/Inspector/FloorGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FloorGeneration))]
+public class MapGenEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        FloorGeneration floorGen = (FloorGeneration)target;
+        if (DrawDefaultInspector())
+        {
+            if (floorGen.autoUpdate)
+            {
+                floorGen.GenerateTiles();
+            }
+        }
+        if (GUILayout.Button("Generate"))
+        {
+            floorGen.GenerateTiles();
+        }
+    }
+}
