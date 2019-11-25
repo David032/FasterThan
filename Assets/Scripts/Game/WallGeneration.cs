9026c4424ccbad3620dc5ab3ba37e4409cb18d66
Jake <pokepeel15@yahoo.co.uk>
2019-11-25 16:21:05 +0000

    Updated Stuff
    Updated level assests

--- Assets/Scripts/Game/WallGeneration.cs
+++ Assets/Scripts/Game/WallGeneration.cs
@@ -6,15 +6,15 @@ public class WallGeneration : MonoBehaviour
 {
     [Range(1, 2)]
     public int wallNumber = 1;
-    [Range(0, 100)]
+    [Range(-200, 200)]
     public int wallDistanceX = 0;
-    [Range(0, 100)]
+    [Range(-200, 200)]
     public int wallDistanceZ = 0;
-    [Range(1, 100)]
+    [Range(1, 200)]
     public int tileX = 1;
     [Range(1, 10)]
     public int tileY = 4;
-    [Range(1, 100)]
+    [Range(1, 200)]
     public int tileZ = 1;
 
     public bool autoUpdate;
@@ -33,16 +33,16 @@ public class WallGeneration : MonoBehaviour
 
         if (wallNumber == 1)
         {
-            walling = Instantiate(Wall, new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z), Quaternion.identity, transform);
-            walling.transform.localScale = new Vector3(0.1f * tileX, 1f * tileY, 0.1f * tileZ);
+            walling = Instantiate(Wall, new Vector3(transform.position.x - (0.05f * wallDistanceX), transform.position.y + 2f, transform.position.z - (0.05f * wallDistanceZ)), Quaternion.identity, transform);
+            walling.transform.localScale = new Vector3(0.05f * tileX, 1f * tileY, 0.05f * tileZ);
         }
         else
         {
-            walling = Instantiate(Wall, new Vector3(transform.position.x - (0.1f * wallDistanceX), transform.position.y + 2f, transform.position.z - (0.1f * wallDistanceZ)), Quaternion.identity, transform);
-            walling.transform.localScale = new Vector3(0.1f * tileX, 1f * tileY, 0.1f * tileZ);
+            walling = Instantiate(Wall, new Vector3(transform.position.x - (0.05f * wallDistanceX), transform.position.y + 2f, transform.position.z - (0.05f * wallDistanceZ)), Quaternion.identity, transform);
+            walling.transform.localScale = new Vector3(0.05f * tileX, 1f * tileY, 0.05f * tileZ);
 
-            walling = Instantiate(Wall, new Vector3(transform.position.x + (0.1f * wallDistanceX), transform.position.y + 2f, transform.position.z + (0.1f * wallDistanceZ)), Quaternion.identity, transform);
-            walling.transform.localScale = new Vector3(0.1f * tileX, 1f * tileY, 0.1f * tileZ);
+            walling = Instantiate(Wall, new Vector3(transform.position.x + (0.05f * wallDistanceX), transform.position.y + 2f, transform.position.z + (0.05f * wallDistanceZ)), Quaternion.identity, transform);
+            walling.transform.localScale = new Vector3(0.05f * tileX, 1f * tileY, 0.05f * tileZ);
         }
     }
 
