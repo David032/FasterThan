05e3640517ce0e66980265726550783ac159408b
Brendon <Brendon2.Thompson@live.uwe.ac.uk>
2019-11-26 13:55:58 +0000

    Highscore System
    integrated the username input and highscore system from the prior game jam, as-well as a basic game manager to use with it.
    
    The Json score system in theory will be able to test at later point!

---
+++ Assets/Scripts/Manager/GameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public float FeatureTime = 20; //How long between feature generations?
+    public float GameTime = 135; //time the game lasts for
+    public float totalScore;
+
+    bool generating;
+
+    string playerName;
+
+    bool muted;
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1 && !generating)
+        {
+            StartCoroutine(EventTimer());
+            StartCoroutine(GameTimer());
+            MuteSceen();
+            generating = true;
+            playerName = "";
+        }
+        else if (SceneManager.GetActiveScene().buildIndex != 1)
+        {
+            generating = false;
+        }
+
+    }
+
+    IEnumerator EventTimer() 
+    {
+        if (!generating) { StopCoroutine(EventTimer()); }
+        yield return new WaitForSeconds(FeatureTime);
+        StartCoroutine(EventTimer());
+    }
+
+    IEnumerator GameTimer()
+    {
+        yield return new WaitForSeconds(GameTime);
+        SceneManager.LoadScene(3);
+    }
+
+    public bool IsMuted()
+    {
+        return muted;
+    }
+
+    public void SetMuted(bool mute)
+    {
+        muted = mute;
+        MuteSceen();
+    }
+
+    void MuteSceen()
+    {
+        var audioSources = FindObjectsOfType<AudioSource>();
+        foreach (var item in audioSources)
+        {
+            item.mute = muted;
+        }
+    }
+
+    public string GetPlayerName()
+    {
+        return playerName;
+    }
+
+    public void SetPlayerName(string newName)
+    {
+        playerName = newName;
+    }
+}
