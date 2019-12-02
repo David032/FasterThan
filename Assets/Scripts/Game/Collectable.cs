408add18f3ce76c201713ff44c27dbd7a9e075a5
Bren <Brendon2.Thompson@live.uwe.ac.uk>
2019-12-02 11:34:09 +0000

    Pickup Prefab added
    Pickup prefab has been added, has 2 lines of code that will need to be un-commented once the scoresystem branch has finally been merged with the game branch to add to the score system!!

---
+++ Assets/Scripts/Game/Collectable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    GameObject manager;
+    float current_game_time;
+
+
+    //Uncomment lines 15 and 22 when ScoreSystem Branch is finally merged!!
+    private void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameController");
+        //current_game_time = manager.GetComponent<GameController>().GameTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            Destroy(gameObject);
+            //manager.GetComponent<GameController>().GameTime = game_time + 15.0f;
+        }
+    }
+}
