0ca92c77affdbf5a4c07448b703fe65825570e6c
Jake <pokepeel15@yahoo.co.uk>
2019-12-02 01:29:19 +0000

    Added in the initial randomize for doors and player spawn
    Added the first iteration of the randomiser for both player spawns and wall pathways. also altered the level to accompany the randomiser.

--- Assets/Scripts/Game/WallSectionRandomiser.cs
+++ Assets/Scripts/Game/WallSectionRandomiser.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class WallSectionRandomiser : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(1, 3)]
+    public int numberOfDoors;
+    public GameObject Door1, Door2, Door3;
+    private int chances = 5;
+    private float yPos = -8.0f;
+
     void Start()
     {
-        
+        Debug.Log("Number of doors: " + numberOfDoors);
+        RandomiserStandardDoors();
     }
 
-    // Update is called once per frame
-    void Update()
+    void RandomiserStandardDoors()
     {
-        
+
+        int doorNumber = 0;
+
+        for (int i = 0; i <= 5; i++)
+        {
+            doorNumber = (Random.Range(0, (numberOfDoors + 1)));
+            Debug.Log("Door Number: " + doorNumber);
+
+            DeleteDoor(doorNumber);
+        }
+    }
+
+    private void DeleteDoor(int doorNum)
+    {
+        if (doorNum == 1)
+        {
+            Door1.transform.localPosition = new Vector3(Door1.transform.localPosition.x, yPos, Door1.transform.localPosition.z);
+        }
+        else if (doorNum == 2)
+        {
+            Door2.transform.localPosition = new Vector3(Door2.transform.localPosition.x, yPos, Door2.transform.localPosition.z);
+        }
+        else if (doorNum == 3)
+        {
+            Door3.transform.localPosition = new Vector3(Door3.transform.localPosition.x, yPos, Door3.transform.localPosition.z);
+        }
     }
 }
