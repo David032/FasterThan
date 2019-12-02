8ccaa04c1473612b7374dc069d964ecbd046a603
si <simon4.harris@live.uwe.ac.uk>
2019-12-02 11:21:14 +0000

    Door trigger basic
    added a basic format for the door trigger which can be added to when level is complete :)

---
+++ Assets/Scripts/Game/DoorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player Entered the trigger");  
+        //set trigger to change level or go to Highscore after level is complete.
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Player is within trigger");  
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Object Exited the trigger");    
+    }
+
+}
