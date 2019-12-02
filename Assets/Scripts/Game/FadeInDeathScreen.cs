bb6d65693f89ee391fba94097807e1fc11d0a7bd
Chloe Brown <chloe5.brown@live.uwe.ac.uk>
2019-12-02 17:17:04 +0000

    You died screen fades in

--- Assets/Scripts/Game/FadeInDeathScreen.cs
+++ Assets/Scripts/Game/FadeInDeathScreen.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class FadeInDeathScreen : MonoBehaviour
 {
-    public CanvasGroup uiElement;
     public GameObject player;
+    public float Duration = 0.4f;
 
-    private void Start()
-    {
-        FadeIn();
+    void Update()
+    { 
+        FadeIn();   
     }
     public void FadeIn()
     {
+        var uiElement = GetComponent<CanvasGroup>();
         if(player.GetComponent<Player_Movement>().canMove == false)
         {
-            StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 1));
+            Debug.Log("Go");
+            //StartCoroutine(FadeCanvasGroup(uiElement, uiElement.alpha, 1));
+            StartCoroutine(DoFade(uiElement, uiElement.alpha, 1));
         }
         
     }
 
+    public IEnumerator DoFade(CanvasGroup canvGroup, float start, float end)
+    {
+        float counter = 0f;
+
+        while (counter < Duration)
+        {
+            counter += Time.deltaTime;
+            canvGroup.alpha = Mathf.Lerp(start, end, counter / Duration);
+            yield return null;
+        }
+    }
     public IEnumerator FadeCanvasGroup(CanvasGroup cg, float start, float end, float lerpTime = 0.5f)
     {
         yield return new WaitForSeconds(1f);
