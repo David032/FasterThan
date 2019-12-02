46e7534d8d52f260f05384edc71fc8e03275b736
si <simon4.harris@live.uwe.ac.uk>
2019-12-02 11:33:48 +0000

    comments
    added comments for other users.

--- Assets/Scripts/Game/DoorTrigger.cs
+++ Assets/Scripts/Game/DoorTrigger.cs
@@ -8,16 +8,19 @@ public class DoorTrigger : MonoBehaviour
     {
         Debug.Log("Player Entered the trigger");  
         //set trigger to change level or go to Highscore after level is complete.
+        //can add anim here for player
     }
 
     void OnTriggerStay(Collider other)
     {
         Debug.Log("Player is within trigger");  
+        //added this incase wqe needed a certain amount of time at the door e.g (takes 5 seconds to open)
     }
 
     void OnTriggerExit(Collider other)
     {
-        Debug.Log("Object Exited the trigger");    
+        Debug.Log("Player Exited the trigger");    
+        //debug check for error checks. if player leaves trigger.
     }
 
 }
