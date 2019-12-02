fb68d832f5ea67bd3e7f204ab9622f7f6ae113b9
Jake <pokepeel15@yahoo.co.uk>
2019-12-02 01:56:51 +0000

    Added a new randomiser - WIP
    Added new script to try and fix an issue

---
+++ Assets/Scripts/Game/WallRandomiser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallRandomiser : MonoBehaviour
+{
+    [Range(0, 2)]
+    private int chances = 0;
+    private float yPos = -8.0f;
+
+    void Start()
+    {
+        RandomiserStandardWall();
+    }
+
+    void RandomiserStandardWall()
+    {
+        int doorNumber = 0;
+        doorNumber = (Random.Range(0, (chances + 1)));
+
+        DeleteDoor();
+    }
+
+    private void DeleteDoor()
+    {
+
+        
+            transform.localPosition = new Vector3(transform.localPosition.x, yPos, transform.localPosition.z);
+       
+    }
+}
