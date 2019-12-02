144e06679a8e4ebc6542b35cbc0472c0735958a8
David Kelly <davek032@outlook.com>
2019-12-02 19:58:59 +0000

    Level improvements
    Slightly tweaked the layout to open up all areas
    All areas are now properly navmeshed
    Exit spot set up as a prefab, with script to report to report it to scoring/other systems

--- Assets/Scripts/Game/DoorTrigger.cs
+++ Assets/Scripts/Game/DoorTrigger.cs
@@ -6,21 +6,9 @@ public class DoorTrigger : MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Player Entered the trigger");  
-        //set trigger to change level or go to Highscore after level is complete.
-        //can add anim here for player
+        if (other.tag.Equals("Player"))
+        {
+            Debug.Log("Player Entered the trigger");
+        }
     }
-
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Player is within trigger");  
-        //added this incase wqe needed a certain amount of time at the door e.g (takes 5 seconds to open)
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        Debug.Log("Player Exited the trigger");    
-        //debug check for error checks. if player leaves trigger.
-    }
-
 }
