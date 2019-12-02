83433e54147823b4c7e2429235210e0a69edde23
Szymon <wlodaczyk_szymon@wp.plk>
2019-12-02 15:46:39 +0000

    updated script

--- Assets/ClarityScript.cs
+++ Assets/ClarityScript.cs
@@ -16,7 +16,7 @@ public class ClarityScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        pointsOfClarity = GameObject.FindGameObjectsWithTag("Tourch");
     }
 
     // Update is called once per frame
