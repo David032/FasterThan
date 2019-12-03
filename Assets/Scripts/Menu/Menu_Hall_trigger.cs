d9ef6bd012adf31e613d0af47a6f5ad88d133490
Jake <pokepeel15@yahoo.co.uk>
2019-12-03 18:11:37 +0000

    Added the Endless Hallway to Menu

---
+++ Assets/Scripts/Menu/Menu_Hall_trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Hall_trigger : MonoBehaviour
+{
+    GameObject MainCam;
+    public GameObject Hallway;
+
+    void Start()
+    {
+        MainCam = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+            Hallway.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 10);
+    }
+}
